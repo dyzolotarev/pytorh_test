3e3fec80555f534d8b2806bbe8e3fbf33ec59a5b
dzolotarev <dzolotarev@gmail.com>
2020-07-15 23:45:05 +0300

    lesson 6 overfitting

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ from torchvision import datasets, transforms
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from torch.utils.data import DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
 
 as_MNIST_transform = transforms.Compose([
     transforms.Resize((28, 28)),
@@ -19,6 +22,34 @@ basic_transform = transforms.Compose([
     # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 ])
 
+mnist_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+])
+
+def mnist2(batch_size=50, valid=0, shuffle=True, transform=mnist_transform, path='./MNIST_data'):
+    test_data = datasets.MNIST(path, train=False, download=True, transform=transform)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
+    
+    train_data = datasets.MNIST(path, train=True, download=True, transform=transform)
+    if valid > 0:
+        num_train = len(train_data)
+        indices = list(range(num_train))
+        split = num_train-valid
+        np.random.shuffle(indices)
+
+        train_idx, valid_idx = indices[:split], indices[split:]
+        train_sampler = SubsetRandomSampler(train_idx)
+        valid_sampler = SubsetRandomSampler(valid_idx)
+
+        train_loader = DataLoader(train_data, batch_size=batch_size, sampler=train_sampler)
+        valid_loader = DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler)
+    
+        return train_loader, valid_loader, test_loader
+    else:
+        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=shuffle)
+        return train_loader, test_loader
+    
 
 def mnist(batch_size=50, transform=basic_transform, path='./MNIST_data'):
     train_data = datasets.MNIST(path, train=True, download=True, transform=transform)
